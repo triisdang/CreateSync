dc018cda2b9dde5eaf4e42eb77d96e28127f42c7
Chipz <roroxji.jk.k5d678@bumpmail.io>
2025-03-15 13:55:50 +0700

    New plan

--- pull.py
+++ pull.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import git
-
+import subprocess
 load_dotenv()
 app_version = "1.0.0"
 docurl = "[WIP]"
@@ -15,17 +15,10 @@ if not GITHUBTOKEN or not GITHUBUSER or not GITHUBLINKURL:
 
 REPO_URL = f"https://{GITHUBUSER}:{GITHUBTOKEN}@github.com/{GITHUBUSER}/{GITHUBLINKURL}.git"
 
-print("Have you use CreateSync before? [Y/N]")
-userinput = input("")
-if userinput == "Y" :
-    print(f"Read {docurl} to Sync your data")
-    userinput =input("")
-elif userinput == "N" :
-    print("Cloning GitHub repo...")
-    try:
-        git.Repo.clone_from(REPO_URL, "./CONFIG")
-        print("Clone successful!")
-    except Exception as e:
-        print(f"Error cloning repository: {e}")
-else :
-    print(f"Please read {docurl} to use create sync")
+print("Cloning GitHub repo...")
+try:
+    git.Repo.clone_from(REPO_URL, "./CONFIG")
+    print("Clone successful!")
+except Exception as e:
+    print(f"Error cloning repository: {e}")
+subprocess.run("python uh.py")
